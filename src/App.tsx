f29ba9a2c72cd311eafe2ca061fcb088fa70f3a3
mengxiong10 <15623530290@163.com>
2022-08-21 15:08:18 +0800

    feat: browserroute to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Routes } from './Routes';
 import { Theme } from './theme';
 import { queryClient } from './queryClient';
 
 export function App() {
+  // 如果用 BrowserRouter 需要 basename
+  // const basename = import.meta.env.BASE_URL
+  const basename = '/';
+
   return (
     <Theme>
       <QueryClientProvider client={queryClient}>
         <CssBaseline />
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter basename={basename}>
           <Routes />
-        </BrowserRouter>
+        </HashRouter>
         <Toaster />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
